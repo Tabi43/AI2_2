0419191d50c3c3df6f691d7b8e97f74b6a644818
Francesca-Corrao <s5471094@studenti.unige.it>
2023-06-11 11:58:11 +0200

    dijkstra update

--- tutorial/visits_module/src/dijkstra.cpp
+++ tutorial/visits_module/src/dijkstra.cpp
@@ -4,78 +4,92 @@
 #include <limits>
 #include <cmath>
 #include <stack>
+#include <algorithm>
+#include <utility>
+#include <functional>
 using namespace std;
 // Structure to represent a vertex in the graph
-struct Vertex {​​
+struct Vertex {
     int id;
     double distance;
-    Vertex(int _id, double _distance) : id(_id), distance(_distance) {​​}​​
-}​​;
-// Structure to represent an edge in the graph
-struct Edge {​​
+    Vertex(int _id, double _distance) : id(_id), distance(_distance) {}
+    };
+    // Structure to represent an edge in the graph
+struct Edge {
     int source;
     int destination;
     double weight;
-    Edge(int _source, int _destination, double _weight) : source(_source), destination(_destination), weight(_weight) {​​}​​
-}​​;
-class Graph {​​
+    Edge(int _source, int _destination, double _weight) : source(_source), destination(_destination), weight(_weight) {}
+    };
+class Graph {
     int V;  // number of vertices
     vector<vector<Edge>> adjacencyList;
 public:
-    Graph(int v) : V(v), adjacencyList(v) {​​}​​
+    Graph(int v) : V(v), adjacencyList(v) {}
     // Add an edge to the graph
-    void addEdge(int source, int destination, double weight) {​​
+    void addEdge(int source, int destination, double weight) {
         adjacencyList[source].emplace_back(source, destination, weight);
         adjacencyList[destination].emplace_back(destination, source, weight);
-    }​​
+    }
     // Dijkstra's algorithm implementation
-    pair<vector<double>, vector<int>> dijkstra(int source) {​​
+    pair<double, vector<int>> dijkstra(int source, int target) {
         vector<double> distances(V, numeric_limits<double>::max());
         distances[source] = 0.0;
         // Create a min-heap priority queue of vertices (ordered by distance)
         priority_queue<Vertex, vector<Vertex>, function<bool(const Vertex&, const Vertex&)>> pq(
-            [](const Vertex& v1, const Vertex& v2) {​​
+            [](const Vertex& v1, const Vertex& v2) {
                 return v1.distance > v2.distance;
-            }​​
+            }
         );
         // Add the source vertex to the priority queue
         pq.push(Vertex(source, 0.0));
         // Array to store the previous node in the shortest path
         vector<int> path(V, -1);
         // Dijkstra's algorithm loop
-        while (!pq.empty()) {​​
+        while (!pq.empty()) {
             int u = pq.top().id;
             pq.pop();
             // Traverse all neighboring vertices of u
-            for (const auto& edge : adjacencyList[u]) {​​
+            for (const auto& edge : adjacencyList[u]) {
                 int v = edge.destination;
                 double weight = edge.weight;
                 // If a shorter path is found, update the distance and enqueue the vertex
-                if (distances[u] + weight < distances[v]) {​​
+                if (distances[u] + weight < distances[v]) {
                     distances[v] = distances[u] + weight;
                     path[v] = u;
                     pq.push(Vertex(v, distances[v]));
-                }​​
-            }​​
-        }​​
-        // Return the distances and path array
-        return make_pair(distances, path);
-    }​​
-    // Trace the path from the source to the target vertex
-    vector<int> getPath(int source, int target, const vector<int>& path) {​​
+                }
+            }
+        }
+        //compute path to go to target
         vector<int> path_trace;
         int node = target;
         // Follow the path from the target vertex to the source vertex
-        while (node != -1) {​​
+        while (node != -1) {
             path_trace.push_back(node);
             node = path[node];
-        }​​
+        }
+        // Reverse the path to get the correct order
+        reverse(path_trace.begin(), path_trace.end());
+        // Return the distances and path array
+        return make_pair(distances[target], path_trace);
+    }
+
+    // Trace the path from the source to the target vertex
+    vector<int> getPath(int source, int target, const vector<int>& path) {
+        vector<int> path_trace;
+        int node = target;
+        // Follow the path from the target vertex to the source vertex
+        while (node != -1) {
+            path_trace.push_back(node);
+            node = path[node];
+        }
         // Reverse the path to get the correct order
         reverse(path_trace.begin(), path_trace.end());
         return path_trace;
-    }​​
-}​​;
-int main() {
+    }
+};
+/*int main() {
     int V = 28;  // total number of vertices (including the initial position and submission desk)
     Graph g(V);
  
@@ -83,7 +97,7 @@ int main() {
     // ...
  
     // Perform Dijkstra's algorithm from the initial position (0)
-    pair<vector<double>, vector<int>> result = g.dijkstra(0);
+    pair<vector<double>, vector<int>> result = g.dijkstra(0,10);
     vector<double>& distances = result.first;
     vector<int>& path = result.second;
  
@@ -105,4 +119,4 @@ int main() {
  
     return 0;
 }
-
+*/
